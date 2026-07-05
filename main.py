fc7bea66e5f228989160234b0a833242b1e94e48
VovTok <vovat0909@gmail.com>
2026-07-05 18:09:58 +0500

    Revert "Greet Black"

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 def main():
     print("hello everyone")
 
-def greetBlack():
-    print("hello black")
-
 def greetWhite():
     print("hello white")
 
 if __name__ == "__main__":
     main()
-    greetWhite()
-    greetBlack()
